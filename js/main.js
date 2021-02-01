a88c6d3aadbf889ceab89d3e82ede5bafe37f09b
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-02-01 18:29:10 -0500

    Display number button values when pressed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+const calculatorDisplay = document.querySelector('.calculator-display');
+const btnNbrs = document.querySelectorAll('.btn-nbr');
+
+const calculator = {
+    a: null,
+    b: null,
+    operator: null
+}
+
+btnNbrs.forEach(btn => {
+    btn.addEventListener('click', event => {
+        calculatorDisplay.textContent += event.target.textContent;
+        //console.log(event.target.textContent);
+    });
+});
+
 function add(a, b) {
     return a + b;
 }
