b106b03d632febeedf5b9dff13a34590cef98dbc
Alex Herdzik <14323111+alexherdzik@users.noreply.github.com>
2021-02-02 19:08:00 -0500

    Adding operator functionality

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 const calculatorDisplay = document.querySelector('.calculator-display');
 const btnNbrs = document.querySelectorAll('.btn-nbr');
 
-const calculator = {
-    a: null,
-    b: null,
-    operator: null
-}
-
 btnNbrs.forEach(btn => {
     btn.addEventListener('click', event => {
         calculatorDisplay.textContent += event.target.textContent;
         //console.log(event.target.textContent);
     });
 });
+
+const btnOps = document.querySelectorAll('.btn-op');
+
+btnOps.forEach(btn => {
+    btn.addEventListener('click', event => {
+        calculator.a = +calculatorDisplay.textContent;
+        calculator.operator = event.target.dataset.operator;
+        calculatorDisplay.textContent += ` ${event.target.textContent} `;
+    });
+})
+
+const btnClear = document.querySelector('.btn-clear');
+btnClear.addEventListener('click', () => {
+    calculator.a = null;
+    calculator.b = null;
+    calculator.operator = null;
+    calculatorDisplay.textContent = '';
+});
+
+const calculator = {
+    a: null,
+    b: null,
+    operator: null
+}
 
 function add(a, b) {
     return a + b;
